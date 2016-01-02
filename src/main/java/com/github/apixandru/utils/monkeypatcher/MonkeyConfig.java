bc84384e57bd543dd91af06715073c2792ae2046
Alexandru-Constantin Bledea <admin@notbed.com>
2016-01-02 16:27:04 +0200

    enhance patch config

--- src/main/java/com/github/apixandru/utils/monkeypatcher/MonkeyConfig.java
+++ src/main/java/com/github/apixandru/utils/monkeypatcher/MonkeyConfig.java
@@ -1,6 +1,8 @@
 package com.github.apixandru.utils.monkeypatcher;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -11,7 +13,13 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import static javax.xml.xpath.XPathConstants.NODESET;
 import static javax.xml.xpath.XPathConstants.STRING;
 
 /**
@@ -21,16 +29,23 @@ import static javax.xml.xpath.XPathConstants.STRING;
 final class MonkeyConfig {
 
     private static final String LOGGING_PATTERN = "/agent-config/properties/logging.pattern";
+    private static final String CLASSES_PATTERN = "/agent-config/classes/class";
 
     final String loggingPattern;
+    final Map<String, ClassToPatch> classes;
 
-    /**
-     * @param loggingPattern
-     */
-    MonkeyConfig(final String loggingPattern) {
+    MonkeyConfig(final String loggingPattern, final Map<String, ClassToPatch> classes) {
         this.loggingPattern = loggingPattern;
+        this.classes = Collections.unmodifiableMap(classes);
     }
 
+    static class ClassToPatch {
+        final String name;
+
+        ClassToPatch(final String name) {
+            this.name = name;
+        }
+    }
 
     /**
      * @param filename
@@ -46,10 +61,26 @@ final class MonkeyConfig {
 
             final XPath xpath = XPathFactory.newInstance().newXPath();
             final String loggingPattern = (String) xpath.evaluate(LOGGING_PATTERN, doc, STRING);
-            return new MonkeyConfig(loggingPattern);
+
+            final NodeList classes = (NodeList) xpath.evaluate(CLASSES_PATTERN, doc, NODESET);
+            return new MonkeyConfig(loggingPattern, parseClasses(classes));
         } catch (ParserConfigurationException | IOException | SAXException | XPathExpressionException e) {
             throw new IllegalStateException("Bad agent configuration", e);
         }
+    }
+
+    private static Map<String, ClassToPatch> parseClasses(final NodeList classes) {
+        Map<String, ClassToPatch> map = new HashMap<>();
+        for (int i = 0; i < classes.getLength(); i++) {
+            final ClassToPatch parse = parse(classes.item(i));
+            map.put(parse.name.replace('.', '/'), parse);
+        }
+        return map;
+    }
+
+    private static ClassToPatch parse(final Node item) {
+        System.out.println(item);
+        return new ClassToPatch(item.getAttributes().getNamedItem("name").getTextContent());
     }
 
 }
