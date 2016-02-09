954df8a014c9e3856734f8ec59ff2445851954d7
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-02-09 21:49:37 +0200

    add utility

--- src/main/java/com/github/apixandru/utils/XmlUtil.java
+++ src/main/java/com/github/apixandru/utils/XmlUtil.java
@@ -3,6 +3,10 @@ package com.github.apixandru.utils;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
@@ -12,6 +16,9 @@ import java.util.stream.Stream;
  * @since February 09, 2016
  */
 public final class XmlUtil {
+
+    // not thread safe
+    private static final XPath XPATH = XPathFactory.newInstance().newXPath();
 
     private XmlUtil() {
 
@@ -27,6 +34,15 @@ public final class XmlUtil {
             nodes.add(nodeList.item(i));
         }
         return nodes.stream();
+    }
+
+    public static Stream<Node> stream(final Node element, final String xPath) {
+        try {
+            final NodeList nodeList = (NodeList) XPATH.evaluate(xPath, element, XPathConstants.NODESET);
+            return streamNodes(nodeList);
+        } catch (XPathExpressionException e) {
+            throw new IllegalArgumentException("XPath failure", e);
+        }
     }
 
 }
