85516cc2085874bdddd3097e71dcfe7dd33a42e1
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-02-09 22:02:15 +0200

    remove implementation

--- src/main/java/com/github/apixandru/utils/monkeypatcher/reimpl/SimpleMethodBodyReplacerConfigParser.java
+++ src/main/java/com/github/apixandru/utils/monkeypatcher/reimpl/SimpleMethodBodyReplacerConfigParser.java
@@ -1,5 +1,6 @@
 package com.github.apixandru.utils.monkeypatcher.reimpl;
 
+import com.github.apixandru.utils.XmlUtil;
 import com.github.apixandru.utils.monkeypatcher.ConfigParser;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static javax.xml.xpath.XPathConstants.NODESET;
 
@@ -41,18 +43,15 @@ public class SimpleMethodBodyReplacerConfigParser implements ConfigParser<Monkey
 
 
     private static ClassToPatch parse(final Node item, final XPath xpath) throws XPathExpressionException {
-        String name = item.getAttributes().getNamedItem("name").getTextContent();
+        final String name = item.getAttributes().getNamedItem("name").getTextContent();
+
+        final List<String> stubs = XmlUtil.stream(item, "class-pool/stub")
+                .map(Node::getTextContent)
+                .collect(Collectors.toList());
+        ;
         return new ClassToPatch(name,
                 parseMethods((NodeList) xpath.evaluate("methods/method", item, NODESET), xpath),
-                parseStubs((NodeList) xpath.evaluate("class-pool/stub", item, NODESET), xpath));
-    }
-
-    private static List<String> parseStubs(final NodeList stubs, final XPath xpath) {
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < stubs.getLength(); i++) {
-            list.add(stubs.item(i).getTextContent());
-        }
-        return list;
+                stubs);
     }
 
     private static Map<String, MethodToPatch> parseMethods(final NodeList methods, final XPath xpath) throws XPathExpressionException {
