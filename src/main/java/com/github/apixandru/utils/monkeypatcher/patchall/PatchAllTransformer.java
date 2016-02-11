25ce97467055361332589d6f8b9613bc6fb8d165
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-02-11 20:02:24 +0200

    add support code

--- src/main/java/com/github/apixandru/utils/monkeypatcher/patchall/PatchAllTransformer.java
+++ src/main/java/com/github/apixandru/utils/monkeypatcher/patchall/PatchAllTransformer.java
@@ -2,8 +2,15 @@ package com.github.apixandru.utils.monkeypatcher.patchall;
 
 import com.github.apixandru.utils.monkeypatcher.AbstractMonkeyPatcher;
 import com.github.apixandru.utils.monkeypatcher.ConfiguredBy;
+import com.github.apixandru.utils.monkeypatcher.Log;
+import javassist.ClassPool;
+import javassist.CtBehavior;
+import javassist.CtClass;
 
+import java.io.ByteArrayInputStream;
 import java.security.ProtectionDomain;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Alexandru-Constantin Bledea
@@ -40,7 +47,31 @@ class PatchAllTransformer extends AbstractMonkeyPatcher<PatchAllConfig> {
     }
 
     private byte[] patch(final String name, final byte[] bytes) {
+        final ClassPool pool = ClassPool.getDefault();
+        final List<CtClass> classes = new ArrayList<>();
+
+        try {
+            final CtClass ctClass = pool.makeClass(new ByteArrayInputStream(bytes));
+            classes.add(ctClass);
+            if (!ctClass.isInterface()) {
+                for (final CtBehavior behavior : ctClass.getDeclaredBehaviors()) {
+                    if (!behavior.isEmpty()) {
+                        patch(behavior);
+                    }
+                }
+                return ctClass.toBytecode();
+            }
+        } catch (Exception e) {
+            Log.error("Failed to patch class", e);
+        } finally {
+            classes.forEach(CtClass::detach);
+        }
+
         return bytes;
+    }
+
+    private void patch(final CtBehavior behavior) {
+
     }
 
 }
