5e053f4187e7da3f2ab1bc435583b0ed5e100125
Alexandru-Constantin Bledea <contact@apixandru.com>
2016-02-11 19:54:21 +0200

    add date to log

--- src/main/java/com/github/apixandru/utils/monkeypatcher/Log.java
+++ src/main/java/com/github/apixandru/utils/monkeypatcher/Log.java
@@ -4,12 +4,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author Alexandru-Constantin Bledea
  * @since February 10, 2016
  */
 public final class Log {
+
+    // safe because it's used in a synchronized manner
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd HH:mm:ss ");
 
     private static FileWriter writer;
 
@@ -36,11 +41,12 @@ public final class Log {
         }
     }
 
-    private static synchronized void writeString(String string) {
+    private static synchronized void writeString(final String string) {
+        final String output = sdf.format(new Date()) + string;
+        System.out.print(output);
         try {
-            System.out.print(string);
             if (null != writer) {
-                writer.write(string);
+                writer.write(output);
                 writer.flush();
             }
         } catch (IOException e) {
