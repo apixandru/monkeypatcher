6c8b729bd0ccfbed129daabfbbecc8995f1a1596
Alexandru-Constantin Bledea <admin@notbed.com>
2016-03-06 16:40:51 +0200

    mock dependencies

--- src/main/java/com/github/apixandru/utils/monkeypatcher/patchall/PatchAllTransformer.java
+++ src/main/java/com/github/apixandru/utils/monkeypatcher/patchall/PatchAllTransformer.java
@@ -13,6 +13,7 @@ import org.slf4j.instrumentation.JavassistHelper;
 import java.io.ByteArrayInputStream;
 import java.security.ProtectionDomain;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -28,7 +29,6 @@ class PatchAllTransformer extends AbstractMonkeyPatcher<PatchAllConfig> {
 
     public byte[] transform(final ClassLoader loader, final String className, final Class clazz,
                             final ProtectionDomain domain, final byte[] bytes) {
-
         if (!config.includes.isEmpty()) {
             for (String include : config.includes) {
 
@@ -52,25 +52,37 @@ class PatchAllTransformer extends AbstractMonkeyPatcher<PatchAllConfig> {
     private byte[] patch(final String name, final byte[] bytes) {
         final ClassPool pool = ClassPool.getDefault();
         final List<CtClass> classes = new ArrayList<>();
-
         try {
-            final CtClass ctClass = pool.makeClass(new ByteArrayInputStream(bytes));
-            classes.add(ctClass);
-            if (!ctClass.isInterface()) {
-                for (final CtBehavior behavior : ctClass.getDeclaredBehaviors()) {
+            final CtClass clasz = pool.makeClass(new ByteArrayInputStream(bytes));
+            classes.add(clasz);
+            mockDependencies(clasz, pool, classes);
+            if (!clasz.isInterface()) {
+                for (final CtBehavior behavior : clasz.getDeclaredBehaviors()) {
                     if (!behavior.isEmpty()) {
                         patch(behavior);
                     }
                 }
-                return ctClass.toBytecode();
+                return clasz.toBytecode();
             }
         } catch (Exception e) {
-            Log.error("Failed to patch class", e);
+            Log.error("Failed to patch " + name, e);
+            System.exit(1);
         } finally {
             classes.forEach(CtClass::detach);
         }
 
         return bytes;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void mockDependencies(final CtClass clasz, final ClassPool classPool, final List<CtClass> classes) {
+        for (String dependency : (Collection<String>) clasz.getRefClasses()) {
+            try {
+                classPool.get(dependency);
+            } catch (javassist.NotFoundException e) {
+                classes.add(classPool.makeClass(dependency));
+            }
+        }
     }
 
     private void patch(final CtBehavior behavior) throws NotFoundException, CannotCompileException {
